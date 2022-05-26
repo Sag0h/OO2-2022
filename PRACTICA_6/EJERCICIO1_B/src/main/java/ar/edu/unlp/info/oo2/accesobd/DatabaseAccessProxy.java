c0f856592095f8722f34d8e79e011173d14993fc
hola <hola@hola.com>
2022-05-26 10:41:42 -0300

    actualizacion ejercicio 1B

--- PRACTICA_6/EJERCICIO1_B/src/main/java/ar/edu/unlp/info/oo2/accesobd/DatabaseAccessProxy.java
+++ PRACTICA_6/EJERCICIO1_B/src/main/java/ar/edu/unlp/info/oo2/accesobd/DatabaseAccessProxy.java
@@ -10,33 +10,10 @@ public class DatabaseAccessProxy implements DatabaseAccess {
 	private boolean logged;
 	private String pass;
 	
-	private void configLogger() {
-		Logger.getLogger("app.main").setUseParentHandlers(false);
-		if(Logger.getLogger("app.main").getHandlers().length < 2 ) {
-			Handler consoleHandler = new ConsoleHandler();
-			consoleHandler.setFormatter(new ShoutingSimpleFormatter());
-			Logger.getLogger("app.main").addHandler(consoleHandler);
-			
-			
-			try {
-				Handler fileHandler = new FileHandler("output_data/mylogs.json");
-				fileHandler.setFormatter(new JSONFormatter());
-				Logger.getLogger("app.main").addHandler(fileHandler);
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		// este if es porque JUnit ejecuta muchas veces mi constructor del AccessProxy 
-		// y crea muchos handlers que no son necesarios
-	}
-	
 	public DatabaseAccessProxy(String pass, DatabaseAccess database) {
 		this.logged = false;
 		this.pass = pass;
 		this.database = database;
-		configLogger();
 	}
 	
 	public boolean login(String password) {
